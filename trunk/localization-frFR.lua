1dff46dda6f3f7eaac52da85075c8a3cf74fb967
tiker <tiker@f089955e-1e60-4c2b-915d-3f3d5fcf5afa>
2010-11-10 00:07:22 +0000

    - Incomplete translations for other languages don't default to english because of the way the tables are being set.  This is a test fix for frFR.

--- trunk/localization-frFR.lua
+++ trunk/localization-frFR.lua
@@ -1,4 +1,5 @@
 if GetLocale() == "frFR" then
+
 	BINDING_HEADER_GOGOHEADER = "GoGoMount" -- Needs review
 	BINDING_NAME_GOGOBINDING = "Monter/descendre de la monture" -- Needs review
 	BINDING_NAME_GOGOBINDING2 = "Monter/descendre de la monture non volante" -- Needs review
@@ -10,17 +11,22 @@ if GetLocale() == "frFR" then
 				Tailoring = "Couture", -- Needs review
 			},
 			String = {
+				AutomaticallyRemoveBuffs = "Automatically remove buffs that prevent mounting", -- Requires localization
 				CurrentZoneFavorites = "Favoris de la zone actuelle", -- Needs review
 				DisableUnknownMountNotices = "Désactiver les annonces de monture inconnue", -- Needs review
 				DisableUpdateNotices = "Désactiver les annonces de mise à jour", -- Needs review
-				-- DruidFlightPreference = "",
+				DruidFlightPreference = "Always use flight forms instead of when moving only", -- Requires localization
+				DruidOptions = "Druid Options", -- Requires localization
 				DruidSingleClick = "Clic pour passer à la monture suivante", -- Needs review
 				EnableAutoDismount = "Activer la descente de monture automatique", -- Needs review
 				Favorites = "Favoris", -- Needs review
 				GlobalFavorites = "Favoris globaux", -- Needs review
 				GlobalPreferences = "Préférences globales", -- Needs review
+				HunterOptions = "Hunter Options", -- Requires localization
 				NewVersionFound = "Une mise à jour de GoGoMount est disponible et est utilisée par", -- Needs review
+				NoShapeInRandom = "Do not randomize shape forms with mounts", -- Requires localization
 				UnknownMountFound = "Une monture inconnue a été trouvée dans votre liste. Veuillez la communiquer afin qu'elle soit ajoutée à une future version de GoGoMount", -- Needs review
+				UseAspectOfThePackInstead = "Use Aspect Of The Pack instead of Aspect Of The Cheeta", -- Requires localization
 			},
 			Talent = {
 				AncestralSwiftness = "Rapidité ancestrale", -- Needs review
@@ -28,24 +34,26 @@ if GetLocale() == "frFR" then
 			},
 			Zone = {
 				AQ40 = "Temple d'Ahn'Qiraj", -- Needs review
-				-- CrumblingDepths = "",
+				CrumblingDepths = "Crumbling Depths", -- Requires localization
 				Dalaran = "Dalaran", -- Needs review
-				-- Deepholm = "",
+				Deepholm = "Deepholm", -- Requires localization
 				DireMaul = "Hache-Tripes", -- Needs review
 				Icecrown = "La Couronne de glace", -- Needs review
 				Ironforge = "Forgefer", -- Needs review
 				KrasusLanding = "Aire de Krasus", -- Needs review
 				SholazarBasin = "Bassin de Sholazar", -- Needs review
-				-- TempleOfEarth = "",
+				TempleOfEarth = "Temple of Earth", -- Requires localization
 				TheFrozenSea = "La mer Gelée", -- Needs review
 				TheOculus = "L'Oculus", -- Needs review
 				TheStormPeaks = "Les pics Foudroyés", -- Needs review
+				TheTempleOfAtalHakkar = "The Temple of Atal'Hakkar", -- Requires localization
 				TheUnderbelly = "Les Entrailles", -- Needs review
 				TheVioletCitadel = "Citadelle Pourpre", -- Needs review
-				-- TolBaradPeninsula = "",
+				TolBarad = "Tol Barad", -- Requires localization
+				TolBaradPeninsula = "Tol Barad Peninsula", -- Requires localization
 				TwistingNether = "Le Néant distordu", -- Needs review
-				-- Vashjir = "",
+				Vashjir = "Vashj'ir", -- Requires localization
 				Wintergrasp = "Joug-d'hiver", -- Needs review
 			},
-	}
+}
 end --if
