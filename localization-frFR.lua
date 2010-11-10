ebbba866c64cc846579116918d541db12d858500
tiker <tiker@funkymonkey.org>
2010-11-10 16:42:00 +0000

    - fixed accent characters for frFR localization - Dire Maul area got rezoned to be part of Azeroth and is now flyable - Small part of Dire Maul that didn't get rezoned and is still it's own entity is also flyable - fixed (not) flyable flag for battle grounds

--- localization-frFR.lua
+++ localization-frFR.lua
@@ -1,36 +1,35 @@
-if GetLocale() == "frFR" then
-
+ï»¿if GetLocale() == "frFR" then
 	BINDING_HEADER_GOGOHEADER = "GoGoMount" -- Needs review
 	BINDING_NAME_GOGOBINDING = "Monter/descendre de la monture" -- Needs review
 	BINDING_NAME_GOGOBINDING2 = "Monter/descendre de la monture non volante" -- Needs review
-	BINDING_NAME_GOGOBINDING3 = "Monter/descendre de la monture à passager" -- Needs review
+	BINDING_NAME_GOGOBINDING3 = "Monter/descendre de la monture Ã  passager" -- Needs review
 	GoGo_Variables.Localize = {
 			Skill = {
-				Engineering = "Ingénierie", -- Needs review
+				Engineering = "IngÃ©nierie", -- Needs review
 				Riding = "Monte", -- Needs review
 				Tailoring = "Couture", -- Needs review
 			},
 			String = {
 				AutomaticallyRemoveBuffs = "Automatically remove buffs that prevent mounting", -- Requires localization
 				CurrentZoneFavorites = "Favoris de la zone actuelle", -- Needs review
-				DisableUnknownMountNotices = "Désactiver les annonces de monture inconnue", -- Needs review
-				DisableUpdateNotices = "Désactiver les annonces de mise à jour", -- Needs review
+				DisableUnknownMountNotices = "DÃ©sactiver les annonces de monture inconnue", -- Needs review
+				DisableUpdateNotices = "DÃ©sactiver les annonces de mise Ã  jour", -- Needs review
 				DruidFlightPreference = "Always use flight forms instead of when moving only", -- Requires localization
 				DruidOptions = "Druid Options", -- Requires localization
-				DruidSingleClick = "Clic pour passer à la monture suivante", -- Needs review
+				DruidSingleClick = "Clic pour passer Ã  la monture suivante", -- Needs review
 				EnableAutoDismount = "Activer la descente de monture automatique", -- Needs review
 				Favorites = "Favoris", -- Needs review
 				GlobalFavorites = "Favoris globaux", -- Needs review
-				GlobalPreferences = "Préférences globales", -- Needs review
+				GlobalPreferences = "PrÃ©fÃ©rences globales", -- Needs review
 				HunterOptions = "Hunter Options", -- Requires localization
-				NewVersionFound = "Une mise à jour de GoGoMount est disponible et est utilisée par", -- Needs review
+				NewVersionFound = "Une mise Ã  jour de GoGoMount est disponible et est utilisÃ©e par", -- Needs review
 				NoShapeInRandom = "Do not randomize shape forms with mounts", -- Requires localization
-				UnknownMountFound = "Une monture inconnue a été trouvée dans votre liste. Veuillez la communiquer afin qu'elle soit ajoutée à une future version de GoGoMount", -- Needs review
+				UnknownMountFound = "Une monture inconnue a Ã©tÃ© trouvÃ©e dans votre liste. Veuillez la communiquer afin qu'elle soit ajoutÃ©e Ã  une future version de GoGoMount", -- Needs review
 				UseAspectOfThePackInstead = "Use Aspect Of The Pack instead of Aspect Of The Cheeta", -- Requires localization
 			},
 			Talent = {
-				AncestralSwiftness = "Rapidité ancestrale", -- Needs review
-				FeralSwiftness = "Célérité farouche", -- Needs review
+				AncestralSwiftness = "RapiditÃ© ancestrale", -- Needs review
+				FeralSwiftness = "CÃ©lÃ©ritÃ© farouche", -- Needs review
 			},
 			Zone = {
 				AQ40 = "Temple d'Ahn'Qiraj", -- Needs review
@@ -43,15 +42,15 @@ if GetLocale() == "frFR" then
 				KrasusLanding = "Aire de Krasus", -- Needs review
 				SholazarBasin = "Bassin de Sholazar", -- Needs review
 				TempleOfEarth = "Temple of Earth", -- Requires localization
-				TheFrozenSea = "La mer Gelée", -- Needs review
+				TheFrozenSea = "La mer GelÃ©e", -- Needs review
 				TheOculus = "L'Oculus", -- Needs review
-				TheStormPeaks = "Les pics Foudroyés", -- Needs review
+				TheStormPeaks = "Les pics FoudroyÃ©s", -- Needs review
 				TheTempleOfAtalHakkar = "The Temple of Atal'Hakkar", -- Requires localization
 				TheUnderbelly = "Les Entrailles", -- Needs review
 				TheVioletCitadel = "Citadelle Pourpre", -- Needs review
 				TolBarad = "Tol Barad", -- Requires localization
 				TolBaradPeninsula = "Tol Barad Peninsula", -- Requires localization
-				TwistingNether = "Le Néant distordu", -- Needs review
+				TwistingNether = "Le NÃ©ant distordu", -- Needs review
 				Vashjir = "Vashj'ir", -- Requires localization
 				Wintergrasp = "Joug-d'hiver", -- Needs review
 			},
