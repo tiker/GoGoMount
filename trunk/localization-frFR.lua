a8b405a5e7cf34930f29770350aad62e3fc841c7
tiker <tiker@f089955e-1e60-4c2b-915d-3f3d5fcf5afa>
2010-11-11 13:53:40 +0000

    - updated frFR translations

--- trunk/localization-frFR.lua
+++ trunk/localization-frFR.lua
@@ -1,50 +1,50 @@
 ﻿if GetLocale() == "frFR" then
-	BINDING_HEADER_GOGOHEADER = "GoGoMount" -- Needs review
-	BINDING_NAME_GOGOBINDING = "Monter/descendre de la monture" -- Needs review
-	BINDING_NAME_GOGOBINDING2 = "Monter/descendre de la monture non volante" -- Needs review
-	BINDING_NAME_GOGOBINDING3 = "Monter/descendre de la monture à passager" -- Needs review
+	BINDING_HEADER_GOGOHEADER = "GoGoMount"
+	BINDING_NAME_GOGOBINDING = "Monter/descendre de monture"
+	BINDING_NAME_GOGOBINDING2 = "Monter/descendre de monture (non volante)"
+	BINDING_NAME_GOGOBINDING3 = "Monter/descendre de monture avec passager"
 	GoGo_Variables.Localize = {
 			Skill = {
-				Engineering = "Ingénierie", -- Needs review
-				Riding = "Monte", -- Needs review
-				Tailoring = "Couture", -- Needs review
+				Engineering = "Ingénierie",
+				Riding = "Monte",
+				Tailoring = "Couture",
 			},
 			String = {
-				AutomaticallyRemoveBuffs = "Automatically remove buffs that prevent mounting", -- Requires localization
-				CurrentZoneFavorites = "Favoris de la zone actuelle", -- Needs review
-				DisableUnknownMountNotices = "Désactiver les annonces de monture inconnue", -- Needs review
-				DisableUpdateNotices = "Désactiver les annonces de mise à jour", -- Needs review
+				AutomaticallyRemoveBuffs = "Supprimer automatiquement les buffs qui empêchent les montures",
+				CurrentZoneFavorites = "Favoris de la zone actuelle",
+				DisableUnknownMountNotices = "Désactiver les annonces de monture inconnue",
+				DisableUpdateNotices = "Désactiver les annonces de mise à jour",
 				DruidFlightPreference = "Always use flight forms instead of when moving only", -- Requires localization
-				DruidOptions = "Druid Options", -- Requires localization
-				DruidSingleClick = "Clic pour passer à la monture suivante", -- Needs review
-				EnableAutoDismount = "Activer la descente de monture automatique", -- Needs review
-				Favorites = "Favoris", -- Needs review
-				GlobalFavorites = "Favoris globaux", -- Needs review
-				GlobalPreferences = "Préférences globales", -- Needs review
-				HunterOptions = "Hunter Options", -- Requires localization
-				NewVersionFound = "Une mise à jour de GoGoMount est disponible et est utilisée par", -- Needs review
+				DruidOptions = "Options pour Druides",
+				DruidSingleClick = "Passer d'une forme à une forme de voyage en un seul clic",
+				EnableAutoDismount = "Activer la descente de monture automatique",
+				Favorites = "Favoris",
+				GlobalFavorites = "Favoris globaux",
+				GlobalPreferences = "Préférences globales",
+				HunterOptions = "Options pour Chasseurs",
+				NewVersionFound = "Une nouvelle version de GoGoMount est disponible",
 				NoShapeInRandom = "Do not randomize shape forms with mounts", -- Requires localization
-				UnknownMountFound = "Une monture inconnue a été trouvée dans votre liste. Veuillez la communiquer afin qu'elle soit ajoutée à une future version de GoGoMount", -- Needs review
-				UseAspectOfThePackInstead = "Use Aspect Of The Pack instead of Aspect Of The Cheeta", -- Requires localization
+				UnknownMountFound = "Une monture inconnue a été trouvée dans votre liste. Veuillez la communiquer afin qu'elle soit ajoutée à une future version de GoGoMount",
+				UseAspectOfThePackInstead = "Utiliser Aspect de la meute au lieu d'Aspect du guépard",
 			},
 			Talent = {
-				AncestralSwiftness = "Rapidité ancestrale", -- Needs review
-				FeralSwiftness = "Célérité farouche", -- Needs review
+				AncestralSwiftness = "Rapidité ancestrale",
+				FeralSwiftness = "Célérité farouche",
 			},
 			Zone = {
 				AQ40 = "Temple d'Ahn'Qiraj", -- Needs review
 				CrumblingDepths = "Crumbling Depths", -- Requires localization
-				Dalaran = "Dalaran", -- Needs review
+				Dalaran = "Dalaran",
 				Deepholm = "Deepholm", -- Requires localization
 				DireMaul = "Hache-Tripes", -- Needs review
-				Icecrown = "La Couronne de glace", -- Needs review
-				Ironforge = "Forgefer", -- Needs review
+				Icecrown = "La Couronne de glace",
+				Ironforge = "Forgefer",
 				KrasusLanding = "Aire de Krasus", -- Needs review
 				SholazarBasin = "Bassin de Sholazar", -- Needs review
 				TempleOfEarth = "Temple of Earth", -- Requires localization
 				TheFrozenSea = "La mer Gelée", -- Needs review
 				TheOculus = "L'Oculus", -- Needs review
-				TheStormPeaks = "Les pics Foudroyés", -- Needs review
+				TheStormPeaks = "Les pics Foudroyés",
 				TheTempleOfAtalHakkar = "The Temple of Atal'Hakkar", -- Requires localization
 				TheUnderbelly = "Les Entrailles", -- Needs review
 				TheVioletCitadel = "Citadelle Pourpre", -- Needs review
@@ -52,7 +52,7 @@
 				TolBaradPeninsula = "Tol Barad Peninsula", -- Requires localization
 				TwistingNether = "Le Néant distordu", -- Needs review
 				Vashjir = "Vashj'ir", -- Requires localization
-				Wintergrasp = "Joug-d'hiver", -- Needs review
+				Wintergrasp = "Joug-d'hiver",
 			},
 }
 end --if
