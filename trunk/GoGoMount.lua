62950d443646534dcc44a61494d5b9038fba7ddd
tiker <tiker@f089955e-1e60-4c2b-915d-3f3d5fcf5afa>
2010-12-19 18:33:10 +0000

    - updated toc version to 4.0.40 - disabled testing flag

--- trunk/GoGoMount.lua
+++ trunk/GoGoMount.lua
@@ -32,7 +32,7 @@ function GoGo_OnEvent(event)
 		end --if
 		GoGo_Prefs.UnknownMounts = {}
 		GoGo_Variables.VerMajor, GoGo_Variables.VerMinor, GoGo_Variables.VerBuild = tonumber(GetAddOnMetadata("GoGoMount", "Version"))
-		GoGo_Variables.TestVersion = true
+		GoGo_Variables.TestVersion = false
 		GoGo_Variables.Debug = false
 		_, GoGo_Variables.Player.Class = UnitClass("player")
 		_, GoGo_Variables.Player.Race = UnitRace("player")
