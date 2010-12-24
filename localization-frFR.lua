c2722fee777831e67f06412b482e5af77320e62d
tiker <tiker@funkymonkey.org>
2010-12-24 17:04:32 +0000

    - testing repository

--- localization-frFR.lua
+++ localization-frFR.lua
@@ -3,8 +3,8 @@
 	BINDING_NAME_GOGOBINDING = "Monter/descendre de monture"
 	BINDING_NAME_GOGOBINDING2 = "Monter/descendre de monture (non volante)"
 	BINDING_NAME_GOGOBINDING3 = "Monter/descendre de monture avec passager"
-	GoGo_Variables.Localize = --@localization(locale="frFR", format="lua_table", handle-unlocalized="english", handle-subnamespaces="subtable", namespace="GoGo_Variables\Localize")@
-	
+	GoGo_Variables.Localize =
+	--@localization(locale="frFR", format="lua_table", handle-unlocalized="english", handle-subnamespaces="subtable", namespace="GoGo_Variables\Localize")@
 	,
 }
 end --if
