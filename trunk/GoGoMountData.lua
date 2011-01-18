28f4bcf284b026c01fda5d803f75d4f8e174f795
tiker <tiker@f089955e-1e60-4c2b-915d-3f3d5fcf5afa>
2011-01-18 01:25:01 +0000

    - Druid Travel Form now works in The Vortex Pinnacle - toc version updated to 4.0.50

--- trunk/GoGoMountData.lua
+++ trunk/GoGoMountData.lua
@@ -426,12 +426,12 @@ function GoGo_GetMountDB()
 	-- Spells, shape forms, etc
 		[40120] = {[7] = true, [9] = true, [11] = true, [23] = true, [24] = true, [9998] = true, ["FlightOnly"] = true, [10003]=380},  -- Druid Fast Flight Form
 		[33943] = {[7] = true, [9] = true, [11] = true, [9998] = true, ["FlightOnly"] = true, [10003]=250},  -- Druid Flight Form
-		[783] = {[7] = true, [10002]=140}, -- Druid Travel Form
-		[2645] = {[8] = true, [1000] = true, ["DefaultInstance"] = true, ["TenebrousCavern"] = true, [10002]=130}, -- Shaman Ghost Wolf Form
-		[768] = {[7] = true, [8] = true, [1000] = true, ["DefaultInstance"] = true, [10002]=100},  -- Druid Cat Form
-		[1066] = {[7] = true, [1000] = true, [53] = true, [10001] = 101, [10004] = 101, ["DefaultInstance"] = true},  -- Druid Aqua form
-		[5118] = {[7] = true, [8] = true, ["DefaultInstance"] = true, ["TenebrousCavern"] = true, [10002]=130},  -- Aspect of the Cheeta
-		[13159] = {[7] = true, [8] = true, ["DefaultInstance"] = true, ["TenebrousCavern"] = true, [10002]=130},  -- Aspect of the Pack
+		[783] = {[7] = true, [500] = true, [10002]=140}, -- Druid Travel Form
+		[2645] = {[8] = true, [500] = true, [1000] = true, ["DefaultInstance"] = true, ["TenebrousCavern"] = true, [10002]=130}, -- Shaman Ghost Wolf Form
+		[768] = {[7] = true, [8] = true, [1000] = true, [500] = true, ["DefaultInstance"] = true, [10002]=100},  -- Druid Cat Form
+		[1066] = {[7] = true, [53] = true, [500] = true, [1000] = true, [10001] = 101, [10004] = 101, ["DefaultInstance"] = true},  -- Druid Aqua form
+		[5118] = {[7] = true, [8] = true, [500] = true, ["DefaultInstance"] = true, ["TenebrousCavern"] = true, [10002]=130},  -- Aspect of the Cheeta
+		[13159] = {[7] = true, [8] = true, [500] = true, ["DefaultInstance"] = true, ["TenebrousCavern"] = true, [10002]=130},  -- Aspect of the Pack
 	}
 end --if
 
