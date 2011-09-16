71f9f1816617196eaf1a8543e164e4e7630deb04
tiker <tiker@funkymonkey.org>
2011-09-16 23:27:56 +0000

    -  support for Brazilian Portuguese (ptBR) translations - translations need to be done still

---
+++ localization-ptBR.lua
@@ -0,0 +1,7 @@
+﻿if GetLocale() == "esES" then
+	BINDING_HEADER_GOGOHEADER = "GoGoMount" -- Needs review
+	BINDING_NAME_GOGOBINDING = "Montar/Desmontar" -- Needs review
+	BINDING_NAME_GOGOBINDING2 = "Montar/Desmontar (no voladora)" -- Needs review
+	BINDING_NAME_GOGOBINDING3 = "Montar/Desmontar Monturas de Pasajeros" -- Needs review
+	GoGo_Variables.Localize = --@localization(locale="ptBR", format="lua_table", handle-unlocalized="english", handle-subnamespaces="subtable", namespace="GoGo_Variables/Localize")@
+end --if
