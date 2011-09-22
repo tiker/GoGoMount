d9cf572d6bb568fe0ff6547a53022696d9029c50
tiker <tiker@f089955e-1e60-4c2b-915d-3f3d5fcf5afa>
2011-09-22 00:37:39 +0000

    - mod version updated 4.2.11 - fixed ptBR localization file problem

--- trunk/localization-ptBR.lua
+++ trunk/localization-ptBR.lua
@@ -1,7 +1,7 @@
-﻿if GetLocale() == "esES" then
+﻿if GetLocale() == "ptBR" then
 	BINDING_HEADER_GOGOHEADER = "GoGoMount" -- Needs review
-	BINDING_NAME_GOGOBINDING = "Montar/Desmontar" -- Needs review
-	BINDING_NAME_GOGOBINDING2 = "Montar/Desmontar (no voladora)" -- Needs review
-	BINDING_NAME_GOGOBINDING3 = "Montar/Desmontar Monturas de Pasajeros" -- Needs review
+	BINDING_NAME_GOGOBINDING = "Mount/Dismount"
+	BINDING_NAME_GOGOBINDING2 = "Mount/Dismount (no flying)"
+	BINDING_NAME_GOGOBINDING3 = "Mount/Dismount Passenger Mounts"
 	GoGo_Variables.Localize = --@localization(locale="ptBR", format="lua_table", handle-unlocalized="english", handle-subnamespaces="subtable", namespace="GoGo_Variables/Localize")@
 end --if
