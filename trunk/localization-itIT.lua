186e9e7941e8c9f5f24fc0385251b18df7be31b3
tiker <tiker@f089955e-1e60-4c2b-915d-3f3d5fcf5afa>
2012-04-10 14:53:11 +0000

    - Fixed problem summoning certain mounts where the mount's spell name is the same as the item name used to teach that mount and that item exists in the player's bags or bank.  (To be thoroughly tested.) - Debug log changes to check for IsSubmerged and to eliminate unneeded lines. - New Italian translations (itIT) now supported and automatically updated with changes on GoGoMount's support site (curseforge.com).

---
+++ trunk/localization-itIT.lua
@@ -0,0 +1,10 @@
+﻿-- For translations visit:  /  Per le traduzioni vai a:
+-- http://wow.curseforge.com/addons/gogomount/localization/itIT/
+
+if GetLocale() == "itIT" then
+	BINDING_HEADER_GOGOHEADER = "GoGoMount"
+	BINDING_NAME_GOGOBINDING = "Montare / Smontare" -- Needs review
+	BINDING_NAME_GOGOBINDING2 = "Montare / Smontare (senza volo)" -- Needs review
+	BINDING_NAME_GOGOBINDING3 = "Montare / Smontare Monti passeggeri" -- Needs review
+	GoGo_Variables.Localize = --@localization(locale="itIT", format="lua_table", handle-unlocalized="english", handle-subnamespaces="subtable", namespace="GoGo_Variables/Localize")@
+end --if
