6723112a4c57c4be8c1674e1635e166576ba7cfa
tiker <tiker@funkymonkey.org>
2014-10-17 02:08:58 +0000

    - Should now cancel "Treant Form" and start casting a mount with the single click to mount option enabled for Druids without giving an error about being shape shifted.

--- GoGoMountData.lua
+++ GoGoMountData.lua
@@ -767,6 +767,7 @@ GoGo_Variables.DebuffDB = {
 	91760,	-- Endure the Transformation (from Bones of Transformation)
 	91771,	-- Wisp Form (from Wisp Amulet)
 	98445,  -- Trapped In Amber (from Ancient Amber)  -- requires 2 clicks to mount
+	114282,  -- Treant Form (Druid glyph in Warcraft 6.0)
 	122099,  -- Bamboozled (from /love Meng Meng in SW)
 	
 }
