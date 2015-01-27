c45c1e5aec9a144f2e6ffee790c812f7f7ea3523
tiker <tiker@f089955e-1e60-4c2b-915d-3f3d5fcf5afa>
2015-01-27 02:27:37 +0000

    tagging for release

--- trunk/GoGoMountData.lua
+++ trunk/GoGoMountData.lua
@@ -19,7 +19,7 @@ GoGo_Variables.WaterSpeed = {}
 GoGo_Variables.WaterSurfaceSpeed = {}
 GoGo_Variables.Debug = 0
 GoGo_Variables.DebugTimer = 0
-GoGo_Variables.TestVersion = true
+GoGo_Variables.TestVersion = false
 
 ---------
 function GoGo_GetMountDB()
