8b1086c66fa7c5d4940899c4c684e0ad571b12cc
tiker <tiker@f089955e-1e60-4c2b-915d-3f3d5fcf5afa>
2016-06-01 21:55:19 +0000

    Added new mount "Great Northern Elderhorn"

--- trunk/GoGoMountData.lua
+++ trunk/GoGoMountData.lua
@@ -717,6 +717,8 @@ function GoGo_GetMountDB()
 		[171840] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Coldflame Infernal
 		[213350] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Frostshard Infernal
 
+		[213339] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Great Northern Elderhorn
+
 		
 --		[123456] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Standard Ground Mount template
 --		[123456] = {[9] = true, [38] = true, [300]=true, [301]=true, [330]=true, [400]=true, [402]=true, [403]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10003]=250, [10004]=67},  -- Standard Air & Ground Mount template
