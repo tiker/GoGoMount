c3b9d1a5359bf68fe3b104c153e31522daafe67d
tiker <tiker@f089955e-1e60-4c2b-915d-3f3d5fcf5afa>
2018-07-20 14:01:29 +0000

    Updated TOC version Fixed bug with UI and zone specific favorites Updated Zone ID to Map ID mappings for zones below Stormwind

--- trunk/GoGoZones.lua
+++ trunk/GoGoZones.lua
@@ -10,15 +10,56 @@ function GoGo_ZoneMapIDMigration()
 	
 	if iMapID == 1 then
 	--
+	elseif iMapID == 17 then
+		iZoneID = 19
+		if not GoGo_Prefs.MapIDs[iMapID] then
+			GoGo_Prefs.MapIDs[iMapID] = GoGo_Prefs.Zones[iZoneID]
+		end --if not GoGo_Prefs.MapIDs....
 	elseif iMapID == 36 then
 		iZoneID = 29
-		GoGo_Prefs.MapIDs[iMapID] = GoGo_Prefs.Zones[iZoneID]
+		if not GoGo_Prefs.MapIDs[iMapID] then
+			GoGo_Prefs.MapIDs[iMapID] = GoGo_Prefs.Zones[iZoneID]
+		end --if not GoGo_Prefs.MapIDs....
 	elseif iMapID == 37 then
 		iZoneID = 30
-		GoGo_Prefs.MapIDs[iMapID] = GoGo_Prefs.Zones[iZoneID]
+		if not GoGo_Prefs.MapIDs[iMapID] then
+			GoGo_Prefs.MapIDs[iMapID] = GoGo_Prefs.Zones[iZoneID]
+		end --if not GoGo_Prefs.MapIDs....
+	elseif iMapID == 47 then
+		iZoneID = 34
+		if not GoGo_Prefs.MapIDs[iMapID] then
+			GoGo_Prefs.MapIDs[iMapID] = GoGo_Prefs.Zones[iZoneID]
+		end --if not GoGo_Prefs.MapIDs....
+	elseif iMapID == 49 then
+		iZoneID = 36
+		if not GoGo_Prefs.MapIDs[iMapID] then
+			GoGo_Prefs.MapIDs[iMapID] = GoGo_Prefs.Zones[iZoneID]
+		end --if not GoGo_Prefs.MapIDs....
+	elseif iMapID == 50 then
+		iZoneID = 37
+		if not GoGo_Prefs.MapIDs[iMapID] then
+			GoGo_Prefs.MapIDs[iMapID] = GoGo_Prefs.Zones[iZoneID]
+		end --if not GoGo_Prefs.MapIDs....
+	elseif iMapID == 51 then
+		iZoneID = 38
+		if not GoGo_Prefs.MapIDs[iMapID] then
+			GoGo_Prefs.MapIDs[iMapID] = GoGo_Prefs.Zones[iZoneID]
+		end --if not GoGo_Prefs.MapIDs....
+	elseif iMapID == 52 then
+		iZoneID = 39
+		if not GoGo_Prefs.MapIDs[iMapID] then
+			GoGo_Prefs.MapIDs[iMapID] = GoGo_Prefs.Zones[iZoneID]
+		end --if not GoGo_Prefs.MapIDs....
 	elseif iMapID == 84 then
 		iZoneID = 301
-		GoGo_Prefs.MapIDs[iMapID] = GoGo_Prefs.Zones[iZoneID]
+		if not GoGo_Prefs.MapIDs[iMapID] then
+			GoGo_Prefs.MapIDs[iMapID] = GoGo_Prefs.Zones[iZoneID]
+		end --if not GoGo_Prefs.MapIDs....
+	elseif iMapID == 210 then
+		iZoneID = 673
+		if not GoGo_Prefs.MapIDs[iMapID] then
+			GoGo_Prefs.MapIDs[iMapID] = GoGo_Prefs.Zones[iZoneID]
+		end --if not GoGo_Prefs.MapIDs....
 
 	end --if iMapID...
 end -- function GoGo_ZoneMapIDMigration()
