4da2667dfd7ef53b289e6d4fcbfd3944ac1210ad
tiker <tiker@f089955e-1e60-4c2b-915d-3f3d5fcf5afa>
2018-08-27 01:40:02 +0000

    Added mount "Skullripper"

--- trunk/GoGoMountData.lua
+++ trunk/GoGoMountData.lua
@@ -900,6 +900,7 @@ function GoGo_GetMountDB()
 		-- 8.0.1.26714 (According to Wowhead)
 		[279569] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Swift Albino Raptor
 		[279608] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Lil' Donkey
+		[279611] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Skullripper
 		
 --		[123456] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Standard Ground Mount template
 --		[123456] = {[9] = true, [38] = true, [300]=true, [301]=true, [330]=true, [400]=true, [402]=true, [403]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10003]=250, [10004]=67},  -- Standard Air & Ground Mount template
