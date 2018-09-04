7bb79687804adc8b50ca813a1f078a05f2953800
tiker <tiker@f089955e-1e60-4c2b-915d-3f3d5fcf5afa>
2018-09-04 23:49:11 +0000

    Added mount "Prestigious Bloodforged Courser"

--- trunk/GoGoMountData.lua
+++ trunk/GoGoMountData.lua
@@ -908,6 +908,7 @@ function GoGo_GetMountDB()
 		-- 8.0.1.26788 (According to Wowhead)
 		[280729] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Frenzied Feltalon
 		[280730] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Pureheart Courser
+		[281044] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Prestigious Bloodforged Courser
 
 		
 --		[123456] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Standard Ground Mount template
