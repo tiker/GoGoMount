415a4cfa4d58d88a33990cddb0527ea6efd6b621
tiker <tiker@f089955e-1e60-4c2b-915d-3f3d5fcf5afa>
2018-09-25 16:23:36 +0000

    Added mount "Vicious Black Warsaber"

--- trunk/GoGoMountData.lua
+++ trunk/GoGoMountData.lua
@@ -914,6 +914,10 @@ function GoGo_GetMountDB()
 		-- 8.0.1.27602 (According to Wowhead)
 		[261437] = {[9] = true, [38] = true, [300]=true, [301]=true, [330]=true, [400]=true, [402]=true, [403]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10003]=250, [10004]=67},  -- Mecha-Mogul Mk2
 		[272770] = {[9] = true, [38] = true, [300]=true, [301]=true, [330]=true, [400]=true, [402]=true, [403]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10003]=250, [10004]=67},  -- The Dreadwake
+
+		-- 8.1.0.27826 (According to Wowhead)
+		[281887] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Vicious Black Warsaber
+
 		
 --		[123456] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Standard Ground Mount template
 --		[123456] = {[9] = true, [38] = true, [300]=true, [301]=true, [330]=true, [400]=true, [402]=true, [403]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10003]=250, [10004]=67},  -- Standard Air & Ground Mount template
